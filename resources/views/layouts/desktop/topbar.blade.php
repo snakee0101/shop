65bce9769dccf02abab652cd8e2de72d7cea665e
Danil <snakee0101@gmail.com>
2021-09-16 16:24:22 +0300

    removed unnecessary items from the topbar

--- resources/views/layouts/desktop/topbar.blade.php
+++ resources/views/layouts/desktop/topbar.blade.php
@@ -5,12 +5,8 @@
                     Us</a></div>
             <div class="topbar__item topbar__item--link"><a class="topbar-link" href="{{ route('contacts') }}">Contacts</a>
             </div>
-            <div class="topbar__item topbar__item--link"><a class="topbar-link" href="#">Store Location</a>
-            </div>
             <div class="topbar__item topbar__item--link"><a class="topbar-link" href="track-order.html">Track
                     Order</a></div>
-            <div class="topbar__item topbar__item--link"><a class="topbar-link" href="blog-classic.html">Blog</a>
-            </div>
             <div class="topbar__spring"></div>
             <div class="topbar__item">
                 <div class="topbar-dropdown">
@@ -32,44 +28,6 @@
                             <li><a href="#">£ Pound Sterling</a></li>
                             <li><a href="#">$ US Dollar</a></li>
                             <li><a href="#">₽ Russian Ruble</a></li>
-                        </ul><!-- .menu / end --></div>
-                </div>
-            </div>
-            <div class="topbar__item">
-                <div class="topbar-dropdown">
-                    <button class="topbar-dropdown__btn" type="button">Language: <span
-                            class="topbar__item-value">EN</span>
-                        <svg width="7px" height="5px">
-                            <use xlink:href="images/sprite.svg#arrow-rounded-down-7x5"></use>
-                        </svg>
-                    </button>
-                    <div class="topbar-dropdown__body"><!-- .menu -->
-                        <ul class="menu menu--layout--topbar menu--with-icons">
-                            <li><a href="#">
-                                    <div class="menu__icon"><img
-                                            srcset="images/languages/language-1.png, images/languages/language-1@2x.png 2x"
-                                            src="images/languages/language-1.png" alt=""></div>
-                                    English</a></li>
-                            <li><a href="#">
-                                    <div class="menu__icon"><img
-                                            srcset="images/languages/language-2.png, images/languages/language-2@2x.png 2x"
-                                            src="images/languages/language-2.png" alt=""></div>
-                                    French</a></li>
-                            <li><a href="#">
-                                    <div class="menu__icon"><img
-                                            srcset="images/languages/language-3.png, images/languages/language-3@2x.png 2x"
-                                            src="images/languages/language-3.png" alt=""></div>
-                                    German</a></li>
-                            <li><a href="#">
-                                    <div class="menu__icon"><img
-                                            srcset="images/languages/language-4.png, images/languages/language-4@2x.png 2x"
-                                            src="images/languages/language-4.png" alt=""></div>
-                                    Russian</a></li>
-                            <li><a href="#">
-                                    <div class="menu__icon"><img
-                                            srcset="images/languages/language-5.png, images/languages/language-5@2x.png 2x"
-                                            src="images/languages/language-5.png" alt=""></div>
-                                    Italian</a></li>
                         </ul><!-- .menu / end --></div>
                 </div>
             </div>
