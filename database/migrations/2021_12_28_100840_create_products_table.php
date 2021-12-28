f4c9d55415f74b77c0952bf4b8c220449303c191
Danil <snakee0101@gmail.com>
2021-12-28 12:29:50 +0200

    made structure of products table

--- database/migrations/2021_12_28_100840_create_products_table.php
+++ database/migrations/2021_12_28_100840_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,17 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price');
+            $table->text('guarantee_info');
+            $table->text('payment_info');
             $table->timestamps();
+            $table->foreignId('category_id');
+
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories');
         });
     }
 
