011c079b8d57006df338b7e2c4f8856ca03c5783
Danil <snakee0101@gmail.com>
2021-12-30 16:07:08 +0200

    created product-wishlist pivot table

---
+++ database/migrations/2021_12_30_140422_create_product_wishlist_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductWishlistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_wishlist', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->foreignId('wishlist_id')->references('id')->on('wishlists');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_wishlist');
+    }
+}
