21078303911992304c7b4deabd890225e16034a2
Danil <snakee0101@gmail.com>
2022-01-02 16:47:35 +0200

    Vue - 'add to cart' button has in_cart state

--- resources/js/components/CartButtonComponent.vue
+++ resources/js/components/CartButtonComponent.vue
@@ -1,5 +1,8 @@
 <template>
-    <button @click="addToCart()" class="btn btn-primary product-card__addtocart" type="button">
+    <button @click="addToCart()" disabled class="btn btn-dark product-card__addtocart" type="button" v-if="in_cart">
+        In Cart
+    </button>
+    <button @click="addToCart()" class="btn btn-primary product-card__addtocart" type="button" v-else>
         Add To Cart
     </button>
 </template>
@@ -8,9 +11,15 @@
 export default {
     name: 'CartButtonComponent',
     props: ['product'],
+    data() {
+        return {
+            'in_cart' : false
+        };
+    },
     methods: {
         addToCart() {
             axios.get(`/cart/add/${this.product.id}/1`);
+            this.in_cart = true;
         }
     }
 }
