dc83da8d02fe1eacd8aa2773320be08ccc5d57d1
Danil <snakee0101@gmail.com>
2022-01-04 17:30:23 +0200

    displayed characteristics on the product's page

--- resources/views/product/characteristics.blade.php
+++ resources/views/product/characteristics.blade.php
@@ -3,49 +3,20 @@
 @section('characteristics')
     <div class="spec">
         <h3 class="spec__header">Specification</h3>
+        @foreach($product->characteristics as $char)
             <div class="spec__row">
-                <div class="spec__name">Material</div>
-                <div class="spec__value">Aluminium, Plastic</div>
+                <div class="spec__name">{{ $char->name }}</div>
+                <div class="spec__value">{{ $char->pivot->value }}</div>
             </div>
-            <div class="spec__row">
-                <div class="spec__name">Engine Type</div>
-                <div class="spec__value">Brushless</div>
-            </div>
+        @endforeach
     </div>
-{{--            <div class="spec__row">
-                <div class="spec__name">Battery Voltage</div>
-                <div class="spec__value">18 V</div>
-            </div>
-            <div class="spec__row">
-                <div class="spec__name">Battery Type</div>
-                <div class="spec__value">Li-lon</div>
-            </div>
-            <div class="spec__row">
-                <div class="spec__name">Number of Speeds</div>
-                <div class="spec__value">2</div>
-            </div>
-            <div class="spec__row">
-                <div class="spec__name">Charge Time</div>
-                <div class="spec__value">1.08 h</div>
-            </div>
-            <div class="spec__row">
-                <div class="spec__name">Weight</div>
-                <div class="spec__value">1.5 kg</div>
-            </div>
-        </div>
-        <div class="spec__section"><h4 class="spec__section-title">Dimensions</h4>
+{{--    <div class="spec__section">
+            <h4 class="spec__section-title">Dimensions</h4>
             <div class="spec__row">
                 <div class="spec__name">Length</div>
                 <div class="spec__value">99 mm</div>
             </div>
-            <div class="spec__row">
-                <div class="spec__name">Width</div>
-                <div class="spec__value">207 mm</div>
-            </div>
-            <div class="spec__row">
-                <div class="spec__name">Height</div>
-                <div class="spec__value">208 mm</div>
-            </div>
+            ....
         </div>
         <div class="spec__disclaimer">Information on technical characteristics, the delivery
             set, the country of manufacture and the appearance of the goods is for reference
