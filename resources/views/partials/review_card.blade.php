3f9f8532f6c5abb3ff810be8ebec44bb54b888af
Danil <snakee0101@gmail.com>
2022-01-05 21:33:16 +0200

    review card has a link to specific review page

--- resources/views/partials/review_card.blade.php
+++ resources/views/partials/review_card.blade.php
@@ -5,7 +5,15 @@
         <div class="review__content w-100">
             <div class="review__author d-flex justify-content-between">
                 <div>{{ $review->author->first_name }} {{ $review->author->last_name }}</div>
-                <div class="review__date m-0 mt-1">{{ $review->created_at->format('d F, Y') }}</div>
+                <div class="review__date m-0 mt-1">
+                    {{ $review->created_at->format('d F, Y') }}
+                    <a href="{{ route('review.show', $review) . '#product_tabs' }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+                            <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/>
+                            <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
             <div class="review__rating">
                 <div class="rating">
