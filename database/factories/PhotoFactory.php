d084031b321977cce5af9e4f02ad5fb740c2039e
Danil <snakee0101@gmail.com>
2022-01-06 14:54:47 +0200

    created files needed for Photo model

---
+++ database/factories/PhotoFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Photo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PhotoFactory extends Factory
+{
+    protected $model = Photo::class;
+
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ tests/Unit/PhotoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class PhotoTest extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
