d05a82348c7a855f5d0cf14fe2bb3c07fc797b5e
Danil <snakee0101@gmail.com>
2022-01-08 17:32:45 +0200

    created Report->author relation

--- tests/Unit/ReportTest.php
+++ tests/Unit/ReportTest.php
@@ -4,14 +4,19 @@ namespace Tests\Unit;
 
 use App\Models\Report;
 use App\Models\Review;
+use App\Models\User;
 use Tests\TestCase;
 
 class ReportTest extends TestCase
 {
-    public function test_example()
+    public function test_report_has_an_author()
     {
         $review = Review::factory()->create();
 
-        dd( Report::factory()->withObject($review)->create() );
+        $report = Report::factory()
+                        ->withObject($review)
+                        ->create();
+
+        $this->assertInstanceOf(User::class, $report->author);
     }
 }
