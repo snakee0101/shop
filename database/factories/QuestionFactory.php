2747972ae6729c7ec1e9de222b1a45a753b66712
Danil <snakee0101@gmail.com>
2022-01-10 13:09:24 +0200

    created question factory definition

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,27 +2,22 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use App\Models\Question;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class QuestionFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Question::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'product_id' => Product::factory(),
+            'comment' => $this->faker->sentence,
+            'notify_on_reply' => false,
         ];
     }
 }
