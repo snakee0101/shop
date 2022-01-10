286aa8b3375b5e5d588f98cfdaec818a84239f0e
Danil <snakee0101@gmail.com>
2022-01-10 19:37:46 +0200

    Checkout - changed shipping details fields

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -56,13 +56,7 @@
                         @endguest
 
                         <div class="card mb-lg-0">
-                            <div class="card-body"><h3 class="card-title">Billing details</h3>
-                                <div class="form-group"><label for="checkout-company-name">Company Name <span
-                                            class="text-muted">(Optional)</span></label> <input type="text"
-                                                                                                class="form-control"
-                                                                                                id="checkout-company-name"
-                                                                                                placeholder="Company Name">
-                                </div>
+                            <div class="card-body"><h3 class="card-title">Shipping details</h3>
                                 <div class="form-group"><label for="checkout-country">Country</label> <select
                                         id="checkout-country" class="form-control">
                                         <option>Select a country...</option>
@@ -74,39 +68,29 @@
                                         <option>Germany</option>
                                         <option>Australia</option>
                                     </select></div>
-                                <div class="form-group"><label for="checkout-street-address">Street Address</label>
+
+                                <div class="form-group border-top border-dark"><label for="checkout-street-address">Address</label>
                                     <input type="text" class="form-control" id="checkout-street-address"
                                            placeholder="Street Address"></div>
-                                <div class="form-group"><label for="checkout-address">Apartment, suite, unit etc. <span
-                                            class="text-muted">(Optional)</span></label> <input type="text"
-                                                                                                class="form-control"
-                                                                                                id="checkout-address">
+                                <div class="form-group"><label for="checkout-address">Apartment<span
+                                            class="text-muted"> (Optional)</span></label> <input type="text"
+                                                                                                 class="form-control"
+                                                                                                 id="checkout-address">
                                 </div>
+                                <p class="text-center">OR</p>
+                                <div class="form-group border-bottom border-dark"><label for="checkout-post-office-address">Post office address</label>
+                                    <input type="text" class="form-control" id="checkout-post-office-address"
+                                           placeholder="Post Office Address"></div>
+                                <hr>
+
                                 <div class="form-group"><label for="checkout-city">Town / City</label> <input
                                         type="text" class="form-control" id="checkout-city"></div>
-                                <div class="form-group"><label for="checkout-state">State / County</label> <input
+                                <div class="form-group"><label for="checkout-state">State</label> <input
                                         type="text" class="form-control" id="checkout-state"></div>
                                 <div class="form-group"><label for="checkout-postcode">Postcode / ZIP</label> <input
                                         type="text" class="form-control" id="checkout-postcode"></div>
-                            </div>
-                            <div class="card-divider"></div>
-                            <div class="card-body"><h3 class="card-title">Shipping Details</h3>
-                                <div class="form-group">
-                                    <div class="form-check"><span class="form-check-input input-check"><span
-                                                class="input-check__body"><input class="input-check__input"
-                                                                                 type="checkbox"
-                                                                                 id="checkout-different-address"> <span
-                                                    class="input-check__box"></span> <svg class="input-check__icon"
-                                                                                          width="9px" height="7px"><use
-                                                        xlink:href="/images/sprite.svg#check-9x7"></use></svg> </span></span><label
-                                            class="form-check-label" for="checkout-different-address">Ship to a
-                                            different address?</label></div>
-                                </div>
-                                <div class="form-group"><label for="checkout-comment">Order notes <span
-                                            class="text-muted">(Optional)</span></label> <textarea id="checkout-comment"
-                                                                                                   class="form-control"
-                                                                                                   rows="4"></textarea>
-                                </div>
+                                <div class="form-group"><label for="shipping-date">Shipping date and time</label> <input
+                                        type="text" class="form-control" id="shipping-date"></div>
                             </div>
                         </div>
                     </div>
@@ -146,6 +130,7 @@
                                     </tfoot>
                                 </table>
                                 <div class="payment-methods">
+                                    <h3 class="card-title">Billing details</h3>
                                     <ul class="payment-methods__list">
                                         <li class="payment-methods__item payment-methods__item--active"><label
                                                 class="payment-methods__item-header"><span
@@ -154,27 +139,10 @@
                                                                                          name="checkout_payment_method"
                                                                                          type="radio" checked="checked"> <span
                                                             class="input-radio__circle"></span> </span></span><span
-                                                    class="payment-methods__item-title">Direct bank transfer</span></label>
+                                                    class="payment-methods__item-title">Direct bank transfer using Stripe</span></label>
                                             <div class="payment-methods__item-container">
-                                                <div class="payment-methods__item-description text-muted">Make your
-                                                    payment directly into our bank account. Please use your Order ID as
-                                                    the payment reference. Your order will not be shipped until the
-                                                    funds have cleared in our account.
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="payment-methods__item"><label
-                                                class="payment-methods__item-header"><span
-                                                    class="payment-methods__item-radio input-radio"><span
-                                                        class="input-radio__body"><input class="input-radio__input"
-                                                                                         name="checkout_payment_method"
-                                                                                         type="radio"> <span
-                                                            class="input-radio__circle"></span> </span></span><span
-                                                    class="payment-methods__item-title">Check payments</span></label>
-                                            <div class="payment-methods__item-container">
-                                                <div class="payment-methods__item-description text-muted">Please send a
-                                                    check to Store Name, Store Street, Store Town, Store State / County,
-                                                    Store Postcode.
+                                                <div class="payment-methods__item-description">
+                                                    payment/card details form
                                                 </div>
                                             </div>
                                         </li>
@@ -192,24 +160,9 @@
                                                 </div>
                                             </div>
                                         </li>
-                                        <li class="payment-methods__item"><label
-                                                class="payment-methods__item-header"><span
-                                                    class="payment-methods__item-radio input-radio"><span
-                                                        class="input-radio__body"><input class="input-radio__input"
-                                                                                         name="checkout_payment_method"
-                                                                                         type="radio"> <span
-                                                            class="input-radio__circle"></span> </span></span><span
-                                                    class="payment-methods__item-title">PayPal</span></label>
-                                            <div class="payment-methods__item-container">
-                                                <div class="payment-methods__item-description text-muted">Pay via
-                                                    PayPal; you can pay with your credit card if you don’t have a PayPal
-                                                    account.
-                                                </div>
-                                            </div>
-                                        </li>
                                     </ul>
                                 </div>
-                                <div class="checkout__agree form-group">
+<!--                                <div class="checkout__agree form-group">
                                     <div class="form-check"><span class="form-check-input input-check"><span
                                                 class="input-check__body"><input class="input-check__input"
                                                                                  type="checkbox"
@@ -220,7 +173,7 @@
                                             class="form-check-label" for="checkout-terms">I have read and agree to the
                                             website <a target="_blank" href="terms-and-conditions.html">terms and
                                                 conditions</a>*</label></div>
-                                </div>
+                                </div>-->
                                 <button type="submit" class="btn btn-primary btn-xl btn-block">Place Order</button>
                             </div>
                         </div>
