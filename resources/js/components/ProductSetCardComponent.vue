365e396070fff7b8a67a9601ab2b2ded6d22abe8
Danil <snakee0101@gmail.com>
2022-01-13 19:17:30 +0200

    ProductSetCardComponent displays real product data

--- resources/js/components/ProductSetCardComponent.vue
+++ resources/js/components/ProductSetCardComponent.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="d-inline-block m-3 mb-4" style="max-width: 400px">
+    <div class="d-inline-block m-3 mb-4 border" style="max-width: 400px">
         <div class="d-inline-flex flex-row">
-            <div v-for="product in [1,2]" class="d-inline-flex flex-col">
+            <div v-for="product in product_set_object.products_json" class="d-inline-flex flex-col ml-2">
                 <div>
                     <img src="/images/products/product-2.jpg" style="width: 50px">
-                    <h6 class="mt-2">$100</h6>
+                    <h6 class="mt-2 text-left">${{ product.price }}</h6>
                 </div>
-                <p class="ml-2">product nayutyutyutyume</p>
+                <p class="ml-2 text-left">{{ product.name }}</p>
             </div>
         </div>
         <div class="d-flex flex-row-reverse align-content-center mt-2">
             <cart-button-component style="width: 200px">
 
             </cart-button-component>
-            <h3 class="mr-3 my-0 mt-1">$200</h3>
+            <h3 class="mr-3 my-0 mt-1">${{ product_set_object.products_json[0].price + product_set_object.products_json[1].price }}</h3>
         </div>
     </div>
 </template>
