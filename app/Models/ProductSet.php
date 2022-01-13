5eda2a7dd186f79503ebbb97b6e5674a50777551
Danil <snakee0101@gmail.com>
2022-01-13 16:51:08 +0200

    Product set adds products to json

--- app/Models/ProductSet.php
+++ app/Models/ProductSet.php
@@ -10,8 +10,15 @@ class ProductSet extends Model
     use HasFactory;
     public $timestamps = false;
 
+    protected $appends = ['products_json'];
+
     public function products()
     {
         return $this->belongsToMany(Product::class, 'product_set_product');
+    }
+
+    public function getProductsJsonAttribute()
+    {
+        return $this->products;
     }
 }
--- tests/Unit/ProductSetTest.php
+++ tests/Unit/ProductSetTest.php
@@ -29,4 +29,24 @@ class ProductSetTest extends TestCase
         $this->assertInstanceOf(Product::class, $product_set->products[0]);
         $this->assertCount(2, $product_set->products);
     }
+
+    public function test_product_set_adds_products_to_json()
+    {
+        $product_set = ProductSet::factory()->create();
+
+        $product_1 = Product::factory()->create();
+        $product_2 = Product::factory()->create();
+
+        DB::table('product_set_product')->insert([
+            'product_set_id' => $product_set->id,
+            'product_id' => $product_1->id
+        ]);
+
+        DB::table('product_set_product')->insert([
+            'product_set_id' => $product_set->id,
+            'product_id' => $product_2->id
+        ]);
+
+        $this->assertStringContainsString('products_json', $product_set->fresh()->toJson());
+    }
 }
