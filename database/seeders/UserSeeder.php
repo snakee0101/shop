82df9b10bc3d37fa553b2b11796585980ba3c9bb
Danil <snakee0101@gmail.com>
2022-01-14 16:39:35 +0200

    created special test user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,12 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
-        $users = \App\Models\User::factory()->count(5)->create();
+        $users = User::factory()->count(4)->create();
+
+        User::factory()->create([   //main user
+            'email' => 'test@gmail.com',
+        ]);
     }
 }
