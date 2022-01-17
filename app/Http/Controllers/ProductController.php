6364037b4843d003b9608bb40da4a6d549da1dff
Danil <snakee0101@gmail.com>
2022-01-17 21:44:38 +0200

    when product is viewed visits are saved to db

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,8 +8,16 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    private function visit(Product $product)
+    {
+        if(auth()->check())
+            auth()->user()->visited_products()->attach($product);
+    }
+
     public function description(Product $product)
     {
+        $this->visit($product);
+
         return view('product.description', [
             'product' => $product,
             'product_sets' => ProductSet::whereContainsProduct($product)->get()
@@ -18,6 +26,8 @@ class ProductController extends Controller
 
     public function characteristics(Product $product)
     {
+        $this->visit($product);
+
         return view('product.characteristics', [
             'product' => $product,
             'product_sets' => ProductSet::whereContainsProduct($product)->get()
@@ -26,6 +36,8 @@ class ProductController extends Controller
 
     public function reviews(Product $product)
     {
+        $this->visit($product);
+
         return view('product.reviews', [
             'product' => $product,
             'reviews' => $product->reviews()->latest()->paginate(40),
@@ -35,6 +47,8 @@ class ProductController extends Controller
 
     public function questions(Product $product)
     {
+        $this->visit($product);
+
         return view('product.questions', [
             'product' => $product,
             'questions' => $product->questions()->latest()->paginate(40),
@@ -44,6 +58,8 @@ class ProductController extends Controller
 
     public function videos(Product $product)
     {
+        $this->visit($product);
+
         return view('product.videos', [
             'product' => $product,
             'product_sets' => ProductSet::whereContainsProduct($product)->get()
@@ -52,6 +68,8 @@ class ProductController extends Controller
 
     public function buy_together(Product $product)
     {
+        $this->visit($product);
+
         return view('product.buy_together', [
             'product' => $product,
             'product_sets' => ProductSet::whereContainsProduct($product)->get()
--- tests/Feature/VisitsTest.php
+++ tests/Feature/VisitsTest.php
@@ -2,14 +2,43 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
+use App\Models\User;
+use App\Models\Wishlist;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class VisitsTest extends TestCase
 {
-    public function test_example()
+    private function visit($route, $product, $user)
     {
+        $this->get( route($route, $product) )->assertOk();
 
+        $this->assertDatabaseHas('visited_products', [
+            'user_id' => $user->id,
+            'product_id' => $product->id
+        ]);
+
+        DB::table('visited_products')->delete();
+
+        $this->assertDatabaseCount('visited_products', 0);
+    }
+
+    public function test_when_authenticated_user_accesses_any_product_controller_page_the_visit_is_saved()
+    {
+        $user = User::factory()->create();
+        $product = Product::factory()->create();
+
+        Wishlist::createDefault($user);
+
+        $this->actingAs($user);
+
+        $this->visit('product.description', $product, $user);
+        $this->visit('product.characteristics', $product, $user);
+        $this->visit('product.reviews', $product, $user);
+        $this->visit('product.questions', $product, $user);
+        $this->visit('product.videos', $product, $user);
     }
 }
