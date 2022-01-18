550590852afc829179d0cb62fb585264b8b1846d
Danil <snakee0101@gmail.com>
2022-01-18 12:17:39 +0200

    visited products are shown

--- resources/views/account/visited_products.blade.php
+++ resources/views/account/visited_products.blade.php
@@ -1,1 +1,12 @@
-<p>test visited products</p>
+<div class="block block-products-carousel" data-layout="grid-5">
+    <div class="container d-flex flex-wrap">
+        @foreach(auth()->user()->visited_products as $product)
+            <product-card-component product="{{ $product }}"
+                                    user="{{ auth()->user() }}"
+                                    :key="{{ $product->id }}"
+                                    class="m-2">
+
+            </product-card-component>
+        @endforeach
+    </div>
+</div>
