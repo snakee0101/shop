214a965717735b30c41d713072f3a7c991aeeaeb
Danil <snakee0101@gmail.com>
2022-01-19 15:51:55 +0200

    VisitsTest refactoring

--- tests/Unit/VisitsTest.php
+++ tests/Unit/VisitsTest.php
@@ -12,36 +12,27 @@ class VisitsTest extends TestCase
     public function test_user_contains_visited_products()
     {
        $user = User::factory()->create();
-       $product = Product::factory()->create();
 
        DB::table('visited_products')->insert([
            'user_id' => $user->id,
-           'product_id' => $product->id
+           'product_id' => Product::factory()->create()->id
        ]);
 
-       $this->assertInstanceOf(Product::class, $user->visited_products()->first());
+       $this->assertInstanceOf(Product::class, $user->visited_products[0]);
     }
 
     public function test_a_product_records_visits()
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
+        $this->actingAs($user = User::factory()->create());
+        Product::factory()->create()
+                          ->visit();
 
-        $this->assertEmpty($user->visited_products);
-
-        $product = Product::factory()->create();
-        $product->visit();
-
-        $this->assertCount(1, $user->fresh()->visited_products);
+        $this->assertNotEmpty($user->fresh()->visited_products);
     }
 
     public function test_duplicated_visit_doesnt_raise_an_exception()
     {
-        $user = User::factory()->create();
-
-        $this->actingAs($user);
-
-        $this->assertEmpty($user->visited_products);
+        $this->actingAs($user = User::factory()->create());
 
         $product = Product::factory()->create();
         $product->visit();
@@ -60,11 +51,9 @@ class VisitsTest extends TestCase
 
     public function test_product_visits_are_recorded_with_timestamp()
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
+        $this->actingAs( $user = User::factory()->create() );
 
-        $product = Product::factory()->create();
-        $product->visit();
+        Product::factory()->create()->visit();
 
         $this->assertNotNull( $user->fresh()->visited_products[0]->pivot->created_at );
     }
