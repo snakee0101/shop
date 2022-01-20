0db59e5a152b5455545261f8ac07be174de05c37
Danil <snakee0101@gmail.com>
2022-01-20 15:31:47 +0200

    filled basic OrderFactory fields

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -5,15 +5,25 @@ namespace Database\Factories;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class OrderFactory extends Factory
 {
+    use WithFaker;
     protected $model = Order::class;
 
     public function definition()
     {
         return [
-            //
+            'is_paid' => false,
+            'country' => $this->faker->country(),
+            'address' => $this->faker->streetAddress(),
+            'apartment' => $this->faker->randomNumber(2),
+            'post_office_address' => null,
+            'city' => $this->faker->city(),
+            'state' => $this->faker->city(),
+            'postcode' => $this->faker->postcode(),
+            'shipping_date' => $this->faker->dateTimeBetween('-1 month'),
         ];
     }
 }
--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Unit;
 
+use App\Models\Order;
 use Tests\TestCase;
 
 class OrderTest extends TestCase
 {
     public function test_example()
     {
+        dd( Order::factory()->create() );
         $this->assertTrue(true);
     }
 }
