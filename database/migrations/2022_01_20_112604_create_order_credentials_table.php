b657573eaace537864d5de2d3c356756e8b2de95
Danil <snakee0101@gmail.com>
2022-01-20 13:32:05 +0200

    created order_credentials table migration

---
+++ database/migrations/2022_01_20_112604_create_order_credentials_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderCredentialsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('order_credentials', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->references('id')->on('orders');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone');
+            $table->string('email');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('order_credentials');
+    }
+}
