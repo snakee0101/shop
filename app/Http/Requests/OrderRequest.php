289421210a681383f7bb7c2ee5a2ea46605a25b0
Danil <snakee0101@gmail.com>
2022-01-21 15:08:06 +0200

    simplified OrderRequest rules

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -9,28 +9,23 @@ class OrderRequest extends FormRequest
 {
     public function rules()
     {
-        if(auth()->check())
-            $credentials_rules = [
-                'first_name' => 'alpha',
-                'last_name' => 'alpha',
-                'phone' => 'regex:/\+\d{12}/',
-                'email' => 'email',
-            ];
-        else
-            $credentials_rules = [
-                'first_name' => 'required|alpha',
-                'last_name' => 'required|alpha',
-                'phone' => 'required|regex:/\+\d{12}/',
-                'email' => 'required|email',
-            ];
+        $credentials_rules = [
+            'first_name' => 'alpha',
+            'last_name' => 'alpha',
+            'phone' => 'regex:/\+\d{12}/',
+            'email' => 'email',
+        ];
+
+        if (!auth()->check())
+            $credentials_rules = array_map(fn($item) => 'required|' . $item, $credentials_rules);
 
         return $credentials_rules + ['address' => 'required_without:post_office_address',
-            'apartment' => 'numeric|nullable',
-            'post_office_address' => ['required_without:address', new OrderPostOfficeAddress( request('apartment') )],
-            'city' => 'required',
-            'state' => 'required',
-            'postcode' => 'digits:5',
-            'shipping_date' => 'date_format:Y-m-d H:i:s'
-        ];
+                'apartment' => 'numeric|nullable',
+                'post_office_address' => ['required_without:address', new OrderPostOfficeAddress(request('apartment'))],
+                'city' => 'required',
+                'state' => 'required',
+                'postcode' => 'digits:5',
+                'shipping_date' => 'date_format:Y-m-d H:i:s'
+            ];
     }
 }
