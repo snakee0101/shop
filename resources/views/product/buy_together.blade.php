15d0853a4f673dddb12f4c987529e38909a88c45
Danil <snakee0101@gmail.com>
2022-01-26 17:22:39 +0200

    shown "bought together" products

--- resources/views/product/buy_together.blade.php
+++ resources/views/product/buy_together.blade.php
@@ -1,5 +1,7 @@
 @extends('product.main')
 
 @section('buy_together')
-    <p>buy together data</p>
+    @foreach($bought_together_data as $category_id => $items_in_category)
+        @include('partials.filtering_group', ['filtering_group_1_products' => $items_in_category])
+    @endforeach
 @endsection
