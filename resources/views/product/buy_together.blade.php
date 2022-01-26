2cf9b1a839e6ae57575fbc817d8c377464e48375
Danil <snakee0101@gmail.com>
2022-01-26 17:32:09 +0200

    filtering group partial - created "No Products" message

--- resources/views/product/buy_together.blade.php
+++ resources/views/product/buy_together.blade.php
@@ -1,8 +1,12 @@
 @extends('product.main')
 
 @section('buy_together')
-    @foreach($bought_together_data as $category_id => $items_in_category)
+    @forelse($bought_together_data as $category_id => $items_in_category)
         @include('partials.filtering_group', [ 'products' => $items_in_category,
                                                'group_name' => \App\Models\Category::find($category_id)->name ])
-    @endforeach
+    @empty
+        <div class="alert alert-danger" role="alert">
+            There are no products yet
+        </div>
+    @endforelse
 @endsection
