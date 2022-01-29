c9bfc5428dc99c9e28a17260697b1de9ecc9c7e9
Danil <snakee0101@gmail.com>
2022-01-29 14:10:27 +0200

    integrated JS into admin template

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -264,10 +264,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('admin/plugins/jquery/jquery.min.js')  }} "></script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }} "></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="{{ asset('admin/dist/js/adminlte.min.js') }} "></script>
 </body>
 </html>
