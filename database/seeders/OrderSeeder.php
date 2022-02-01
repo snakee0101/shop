82c1b8e5db04cc2a1ea28744220d782653298ab2
Danil <snakee0101@gmail.com>
2022-02-01 15:05:20 +0200

    fixed order credentials problem in seeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Order;
+use App\Models\OrderCredentials;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -16,6 +17,13 @@ class OrderSeeder extends Seeder
 
         //15 orders of 5 products in each (7 of them are completed)
         $completed_orders = Order::factory()->count(3)->withStatus('completed')->create();
+
+        $completed_orders->each(function($order) {
+            OrderCredentials::factory()->create([
+                'order_id' => $order->id
+            ]);
+        });
+
 
         $users = User::inRandomOrder()->limit(4)->get();
         $users->each(function ($user) use (&$completed_orders){
