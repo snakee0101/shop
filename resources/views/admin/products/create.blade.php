1b6fa649f902b1c39b72cb3e83234f97d3175e82
Danil <snakee0101@gmail.com>
2022-03-04 16:02:26 +0200

    changed "create product" page layout

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -3,98 +3,102 @@
 @section('content')
     <p class="m-3 font-weight-bold"><a href="{{ route('admin.products.index') }}" class="text-danger">&lt; Back to all
             products</a></p>
-    <h2>Create product</h2>
+    <h2 class="my-3 font-weight-bold">Create product</h2>
 
     <form action="{{ route('admin.products.store_product') }}" method="post" enctype="multipart/form-data">
         <div class="container row">
-            <div class="card card-primary col-5 p-0 m-auto">
+            <div class="card card-primary col p-0 m-auto">
                 <div class="card-header">
                     <h3 class="card-title">Basic data</h3>
                 </div>
                 <!-- /.card-header -->
                 <!-- form start -->
                 @csrf
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="product_name">Name</label>
-                        <input type="text" class="form-control" name="name" id="product_name"
-                               placeholder="Enter product name">
-                        @error('name')
-                        <p class="text-danger mt-1">Product name is required</p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="product_description">Description</label>
-                        <textarea class="form-control" name="description" id="product_description"
-                                  placeholder="Enter product description" rows="5">
+                <div class="card-body row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="product_name">Name</label>
+                            <input type="text" class="form-control" name="name" id="product_name"
+                                   placeholder="Enter product name">
+                            @error('name')
+                            <p class="text-danger mt-1">Product name is required</p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="product_description">Description</label>
+                            <textarea class="form-control" name="description" id="product_description"
+                                      placeholder="Enter product description" rows="5">
 
                         </textarea>
-                        @error('description')
-                        <p class="text-danger mt-1">Product description is required</p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="product_name">Price</label>
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">$</span>
-                            <input name="price" type="text" class="form-control" value="0.00">
+                            @error('description')
+                            <p class="text-danger mt-1">Product description is required</p>
+                            @enderror
                         </div>
-                        @error('price')
-                        <p class="text-danger mt-1">Product price is required</p>
-                        @enderror
+                        <div class="form-group">
+                            <label for="product_name">Price</label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">$</span>
+                                <input name="price" type="text" class="form-control" value="0.00">
+                            </div>
+                            @error('price')
+                            <p class="text-danger mt-1">Product price is required</p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="product_category">Product category</label>
+                            <select class="form-control select2 select2-hidden-accessible" style="width: 100%;"
+                                    name="category_id" tabindex="-1" aria-hidden="true">
+                                <option selected="selected" data-select2-id="0" value="">None</option>
+                                @foreach($categories as $category)
+                                    <option data-select2-id="{{ $category->id }}"
+                                            value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
+                            </select>
+
+                            @error('category_id')
+                            <p class="text-danger mt-1">Product category is required</p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="product_category">Stock status</label>
+                            <div class="custom-control custom-radio custom-control-inline d-inline-block">
+                                <input type="radio" id="status_on_hold" name="in_stock" class="custom-control-input"
+                                       value="{{ \App\Models\Product::STATUS_IN_STOCK }}" checked>
+                                <label class="custom-control-label" for="status_on_hold">In Stock</label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline d-inline-block">
+                                <input type="radio" id="status_processing" name="in_stock"
+                                       class="custom-control-input"
+                                       value="{{ \App\Models\Product::STATUS_ENDS }}">
+                                <label class="custom-control-label" for="status_processing">Ends</label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline d-inline-block">
+                                <input type="radio" id="status_cancelled" name="in_stock" class="custom-control-input"
+                                       value="{{ \App\Models\Product::STATUS_OUT_OF_STOCK }}">
+                                <label class="custom-control-label" for="status_cancelled">Out Of Stock</label>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="product_payment_info">Payment info</label>
-                        <textarea class="form-control" name="payment_info" id="product_payment_info"
-                                  placeholder="Enter product payment info" rows="5">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="product_payment_info">Payment info</label>
+                            <textarea class="form-control" name="payment_info" id="product_payment_info"
+                                      placeholder="Enter product payment info" rows="5">
 
                         </textarea>
-                        @error('payment_info')
-                        <p class="text-danger mt-1">Product payment info is required</p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="product_guarantee_info">Guarantee info</label>
-                        <textarea class="form-control" name="guarantee_info" id="product_guarantee_info"
-                                  placeholder="Enter product guarantee info" rows="5">
+                            @error('payment_info')
+                            <p class="text-danger mt-1">Product payment info is required</p>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="product_guarantee_info">Guarantee info</label>
+                            <textarea class="form-control" name="guarantee_info" id="product_guarantee_info"
+                                      placeholder="Enter product guarantee info" rows="5">
 
                         </textarea>
-                        @error('guarantee_info')
-                        <p class="text-danger mt-1">Product guarantee info is required</p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="product_category">Product category</label>
-                        <select class="form-control select2 select2-hidden-accessible" style="width: 100%;"
-                                name="category_id" tabindex="-1" aria-hidden="true">
-                            <option selected="selected" data-select2-id="0" value="">None</option>
-                            @foreach($categories as $category)
-                                <option data-select2-id="{{ $category->id }}"
-                                        value="{{ $category->id }}">{{ $category->name }}</option>
-                            @endforeach
-                        </select>
-
-                        @error('category_id')
-                        <p class="text-danger mt-1">Product category is required</p>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="product_category">Stock status</label>
-                        <div class="custom-control custom-radio custom-control-inline d-inline-block">
-                            <input type="radio" id="status_on_hold" name="in_stock" class="custom-control-input"
-                                   value="{{ \App\Models\Product::STATUS_IN_STOCK }}" checked>
-                            <label class="custom-control-label" for="status_on_hold">In Stock</label>
-                        </div>
-                        <div class="custom-control custom-radio custom-control-inline d-inline-block">
-                            <input type="radio" id="status_processing" name="in_stock"
-                                   class="custom-control-input"
-                                   value="{{ \App\Models\Product::STATUS_ENDS }}">
-                            <label class="custom-control-label" for="status_processing">Ends</label>
-                        </div>
-                        <div class="custom-control custom-radio custom-control-inline d-inline-block">
-                            <input type="radio" id="status_cancelled" name="in_stock" class="custom-control-input"
-                                   value="{{ \App\Models\Product::STATUS_OUT_OF_STOCK }}">
-                            <label class="custom-control-label" for="status_cancelled">Out Of Stock</label>
+                            @error('guarantee_info')
+                            <p class="text-danger mt-1">Product guarantee info is required</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -108,6 +112,6 @@
             </div>
         </div>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-warning mt-3">Submit</button>
     </form>
 @endsection
