31d609a0c72082478ea6581e827ba0dec5ad982e
Danil <snakee0101@gmail.com>
2022-03-07 14:07:40 +0200

    "Edit product" page - filled the discount form

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -120,7 +120,7 @@
                 <div class="card-header">
                     <h3 class="card-title">Discount</h3>
                     <div class="form-check d-inline ml-4">
-                        <input class="form-check-input" type="checkbox" id="discount_applied" name="discount_applied">
+                        <input class="form-check-input" type="checkbox" id="discount_applied" name="discount_applied" {{ $product->discount()->exists() ? 'checked' : '' }}>
                         <label class="form-check-label" for="discount_applied">
                             Apply discount
                         </label>
@@ -129,17 +129,17 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col d-flex flex-row align-items-center">
-                            - <input name="discount_value" class="form-control mx-2" type="text" value="0.00">
+                            - <input name="discount_value" class="form-control mx-2" type="text" value="{{ $product->discount()->exists() ? number_format($product->discount->value, 2) : '0.00' }}">
                             <div class="custom-control custom-radio custom-control-inline d-inline-block">
                                 <input type="radio" id="fixed_discount" name="discount_classname"
                                        class="custom-control-input"
-                                       value="{{ \App\Discounts\FixedPriceDiscount::class }}" checked>
+                                       value="{{ \App\Discounts\FixedPriceDiscount::class }}" {{ $product->discount()->exists() && $product->discount->discount_classname == \App\Discounts\FixedPriceDiscount::class ? 'checked' : '' }}>
                                 <label class="custom-control-label" for="fixed_discount">$</label>
                             </div>
                             <div class="custom-control custom-radio custom-control-inline d-inline-block">
                                 <input type="radio" id="percent_discount" name="discount_classname"
                                        class="custom-control-input"
-                                       value="{{ \App\Discounts\PercentDiscount::class }}">
+                                       value="{{ \App\Discounts\PercentDiscount::class }}" {{ $product->discount()->exists() && $product->discount->discount_classname == \App\Discounts\PercentDiscount::class ? 'checked' : '' }}>
                                 <label class="custom-control-label" for="percent_discount">%</label>
                             </div>
                         </div>
@@ -154,7 +154,7 @@
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="discount_active_since"
                                    id="discount_active_since"
-                                   placeholder="Enter discount start date. Format: 2020-02-02">
+                                   placeholder="Enter discount start date. Format: 2020-02-02" value="{{ $product->discount()->exists() && $product->discount->active_since ? $product->discount->active_since : '' }}">
                         </div>
                     </div>
                     <div class="row mt-3">
@@ -162,12 +162,12 @@
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="discount_active_until"
                                    id="discount_active_until"
-                                   placeholder="Enter discount expire date. Format: 2020-02-02">
+                                   placeholder="Enter discount expire date. Format: 2020-02-02" value="{{ $product->discount()->exists() && $product->discount->active_until ? $product->discount->active_until : '' }}">
                         </div>
                     </div>
                     <div class="row mt-3">
                         <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="coupon_code" name="with_coupon_code">
+                            <input class="form-check-input" type="checkbox" id="coupon_code" name="with_coupon_code" {{ $product->discount()->exists() && $product->discount->coupon_code ? 'checked' : '' }}>
                             <label class="form-check-label" for="coupon_code">
                                 With coupon code
                             </label>
