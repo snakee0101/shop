e9d1bf7d53c18a89d6a6395f217315c748c85bde
Danil <snakee0101@gmail.com>
2022-03-07 18:44:03 +0200

    created product set controller

---
+++ app/Http/Controllers/ProductSetController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductSet;
+use Illuminate\Http\Request;
+
+class ProductSetController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(ProductSet $productSet)
+    {
+        //
+    }
+
+    public function edit(ProductSet $productSet)
+    {
+        //
+    }
+
+    public function update(Request $request, ProductSet $productSet)
+    {
+        //
+    }
+
+    public function destroy(ProductSet $productSet)
+    {
+        //
+    }
+}
---
+++ tests/Feature/ProductSetTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductSetTest extends TestCase
+{
+    public function test_example()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+}
