3756016f88961eb11f724c473d1a5e8b6ed3d144
Danil <snakee0101@gmail.com>
2022-03-08 12:32:41 +0200

    Admin - product set is created with discount

--- app/Http/Controllers/ProductSetController.php
+++ app/Http/Controllers/ProductSetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\ProductSet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductSetController extends Controller
 {
@@ -29,9 +30,30 @@ class ProductSetController extends Controller
             'product-2' => 'required|exists:products,id',
         ]);
 
-        ProductSet::create()
-                  ->products()
-                  ->attach([ $request['product-1'], $request['product-2'] ]);
+        //Create product set
+        $product_set = ProductSet::create();
+
+        $product_set->products()
+                    ->attach([ $request['product-1'], $request['product-2'] ]);
+
+        //Apply discount
+        if ($request->discount_applied === 'on') {
+            $data = [
+                'discount_classname' => $request->discount_classname,
+                'value' => $request->discount_value,
+                'active_since' => $request->discount_active_since,
+                'active_until' => $request->discount_active_until,
+                'coupon_code' => $request->coupon_code
+            ];
+
+            if ($request->discount_active_until && !$request->discount_active_since)
+                $data['active_since'] = date('Y-m-d');
+
+            if ($request->with_coupon_code === 'on')
+                $data['coupon_code'] = Str::uuid();
+
+            $product_set->discount()->create($data);
+        }
 
         session()->flash('message', 'Product Set has been successfully created');
 
--- tests/Feature/ProductSetTest.php
+++ tests/Feature/ProductSetTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Discounts\FixedPriceDiscount;
 use App\Models\Product;
+use App\Models\ProductSet;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -31,6 +33,23 @@ class ProductSetTest extends TestCase
 
     public function test_product_set_could_be_created_with_discount()
     {
+        $product = Product::factory()->create();
+        $product2 = Product::factory()->create();
 
+        $discount_data = [
+            'discount_applied' => 'on',
+            'discount_classname' => FixedPriceDiscount::class,
+            'discount_value' => 10,
+        ];
+
+        $this->post( route('product_set.store'), [
+            'product-1' => $product->id,
+            'product-2' => $product2->id] + $discount_data)->assertRedirect();
+
+        $this->assertDatabaseHas('discounts', [
+            'discount_classname' => $discount_data['discount_classname'],
+            'value' => $discount_data['discount_value'],
+            'item_type' => ProductSet::class
+        ]);
     }
 }
