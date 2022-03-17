255d960c2a1d4f262f9895134b046e289a66f488
Danil <snakee0101@gmail.com>
2022-03-17 18:32:33 +0200

    created Product resource Controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Product $product)
+    {
+        //
+    }
+
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
