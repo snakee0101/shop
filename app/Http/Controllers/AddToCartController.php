4269345d20fb6abb3223313d2fffb3f2503589dd
Danil <snakee0101@gmail.com>
2022-03-17 17:59:55 +0200

    CartController - unique id is generated with built-in PHP function

--- app/Http/Controllers/AddToCartController.php
+++ app/Http/Controllers/AddToCartController.php
@@ -10,13 +10,11 @@ class AddToCartController extends Controller
 
     public function add($quantity)
     {
-        $unique_micro_timestamp = intval(\microtime(true) * 10000); //it is unique cart id
-
         if ($this->item->in_cart)  //item could be added to cart only once
             return;
 
         \Cart::add(array(
-            'id' => $unique_micro_timestamp,
+            'id' => uniqid(),
             'name' => $this->item->name,
             'price' => $this->item->priceWithDiscount,
             'quantity' => $quantity,
