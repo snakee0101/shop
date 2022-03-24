25df484283ed75cf20c2853d52e0a4b616a7edc8
Danil <snakee0101@gmail.com>
2022-03-24 17:23:25 +0200

    fixed variable names

--- app/Models/Characteristic.php
+++ app/Models/Characteristic.php
@@ -40,10 +40,10 @@ class Characteristic extends Model
         //[ char_id => [char_value_1, char_value_2, ...], ... ]   - extracted only characteristic value
 
         $unique_chars = $grouped_char_values->reject(function ($char_values) {
-            $number_ob_characteristics_in_row = $char_values->count();
+            $number_of_characteristics_in_row = $char_values->count();
             $number_of_duplicates_in_row = $char_values->duplicates()->count() + 1;
 
-            return $number_ob_characteristics_in_row === $number_of_duplicates_in_row;
+            return $number_of_characteristics_in_row === $number_of_duplicates_in_row;
         });  //removed characteristics that have the same value across all products (for example, this will be removed 1 => [5, 5, 5], but this will not - 2 => [4, 4, 3] )
 
         $unique_characteristics_model_ids = $unique_chars->keys(); //extracted characteristics model ids
