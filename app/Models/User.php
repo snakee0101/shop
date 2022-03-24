f6f997516851d1dc6ab6a47e4016a82e5d318eb7
Danil <snakee0101@gmail.com>
2022-03-24 13:49:59 +0200

    user has "photos" relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,4 +78,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Video::class);
     }
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
 }
