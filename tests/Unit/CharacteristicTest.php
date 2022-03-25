146d1a9e8238a0e35604903b8f76841d1788b7b8
Danil <snakee0101@gmail.com>
2022-03-25 12:28:01 +0200

    Unit/CharacteristicTest refactoring

--- tests/Unit/CharacteristicTest.php
+++ tests/Unit/CharacteristicTest.php
@@ -10,59 +10,49 @@ use Tests\TestCase;
 
 class CharacteristicTest extends TestCase
 {
-    public function test_a_product_has_characteristics()
+    private function createCharacteristicsForProduct($product)
     {
         $chars = Characteristic::factory()->count(2)->create();
+
+        DB::table('characteristic_product')->insert([
+            [
+                'id' => 1,
+                'product_id' => $product->id,
+                'characteristic_id' => $chars[0]->id,
+                'value' => 100],
+            [
+                'id' => 2,
+                'product_id' => $product->id,
+                'characteristic_id' => $chars[1]->id,
+                'value' => 'test value']
+        ]);
+    }
+
+    public function test_a_product_has_characteristics()
+    {
         $product = Product::factory()->create();
 
-        DB::table('characteristic_product')->insert([
-            'id' => 1,
-            'product_id' => $product->id,
-            'characteristic_id' => $chars[0]->id,
-            'value' => 100
-        ]);
+        $this->createCharacteristicsForProduct($product);
 
-        DB::table('characteristic_product')->insert([
-            'id' => 2,
-            'product_id' => $product->id,
-            'characteristic_id' => $chars[1]->id,
-            'value' => 'test value'
-        ]);
-
-        $this->assertInstanceOf(Characteristic::class, $product->characteristics[0]);
+        $this->assertInstanceOf(Characteristic::class, $product->characteristics()->first());
     }
 
     public function test_category_knows_its_characteristics()
     {
-        $category = Category::factory()->create();
-
-        $chars = Characteristic::factory()->count(2)->create([
-            'category_id' => $category->id
+        Characteristic::factory()->create([
+            'category_id' => $category = Category::factory()->create()
         ]);
 
-        $this->assertInstanceOf(Characteristic::class, $category->characteristics[0]);
+        $this->assertInstanceOf(Characteristic::class, $category->characteristics()->first());
     }
 
     public function test_characteristics_pivot_table_stores_its_value()
     {
-        $chars = Characteristic::factory()->count(2)->create();
         $product = Product::factory()->create();
 
-        DB::table('characteristic_product')->insert([
-            'id' => 1,
-            'product_id' => $product->id,
-            'characteristic_id' => $chars[0]->id,
-            'value' => 100
-        ]);
+        $this->createCharacteristicsForProduct($product);
 
-        DB::table('characteristic_product')->insert([
-            'id' => 2,
-            'product_id' => $product->id,
-            'characteristic_id' => $chars[1]->id,
-            'value' => 'test value'
-        ]);
-
-        $this->assertEquals(100, $product->characteristics[0]->pivot->value);
+        $this->assertEquals(100, $product->characteristics()->first()->pivot->value);
     }
 
     public function test_characteristics_model_could_retrieve_unique_characteristics_data()
@@ -75,15 +65,15 @@ class CharacteristicTest extends TestCase
         $char2 = Characteristic::factory()->create(['category_id' => $product2->category_id]);
         $char3 = Characteristic::factory()->create(['category_id' => $product3->category_id]);
 
-        DB::table('characteristic_product')->insert(['product_id' => $product1->id, 'characteristic_id' => $char1->id, 'value' => 'unique' ]);
-        DB::table('characteristic_product')->insert(['product_id' => $product2->id, 'characteristic_id' => $char1->id, 'value' => 10 ]);
-        DB::table('characteristic_product')->insert(['product_id' => $product3->id, 'characteristic_id' => $char1->id, 'value' => 0 ]);
+        DB::table('characteristic_product')->insert(['product_id' => $product1->id, 'characteristic_id' => $char1->id, 'value' => 'unique']);
+        DB::table('characteristic_product')->insert(['product_id' => $product2->id, 'characteristic_id' => $char1->id, 'value' => 10]);
+        DB::table('characteristic_product')->insert(['product_id' => $product3->id, 'characteristic_id' => $char1->id, 'value' => 0]);
 
-        DB::table('characteristic_product')->insert(['product_id' => $product1->id, 'characteristic_id' => $char2->id, 'value' => 'same value' ]);
-        DB::table('characteristic_product')->insert(['product_id' => $product2->id, 'characteristic_id' => $char2->id, 'value' => 'same value' ]);
-        DB::table('characteristic_product')->insert(['product_id' => $product3->id, 'characteristic_id' => $char2->id, 'value' => 'same value' ]);
+        DB::table('characteristic_product')->insert(['product_id' => $product1->id, 'characteristic_id' => $char2->id, 'value' => 'same value']);
+        DB::table('characteristic_product')->insert(['product_id' => $product2->id, 'characteristic_id' => $char2->id, 'value' => 'same value']);
+        DB::table('characteristic_product')->insert(['product_id' => $product3->id, 'characteristic_id' => $char2->id, 'value' => 'same value']);
 
-        $this->assertCount(1,Characteristic::diff(Product::all()) );
+        $this->assertCount(1, Characteristic::diff(Product::all()));
     }
 
     public function test_characteristics_diff_returns_valid_characteristics_even_for_one_product()
@@ -93,10 +83,10 @@ class CharacteristicTest extends TestCase
         $char1 = Characteristic::factory()->create(['category_id' => $product1->category_id]);
         $char2 = Characteristic::factory()->create(['category_id' => $product1->category_id]);
 
-        DB::table('characteristic_product')->insert(['product_id' => $product1->id, 'characteristic_id' => $char1->id, 'value' => 'unique' ]);
-        DB::table('characteristic_product')->insert(['product_id' => $product1->id, 'characteristic_id' => $char2->id, 'value' => 'same value' ]);
+        DB::table('characteristic_product')->insert(['product_id' => $product1->id, 'characteristic_id' => $char1->id, 'value' => 'unique']);
+        DB::table('characteristic_product')->insert(['product_id' => $product1->id, 'characteristic_id' => $char2->id, 'value' => 'same value']);
 
-        $this->assertNotEmpty( Characteristic::diff(Product::all()) );
+        $this->assertNotEmpty(Characteristic::diff(Product::all()));
     }
 
     public function test_when_characteristics_are_deleted_all_products_are_detached()
@@ -104,7 +94,7 @@ class CharacteristicTest extends TestCase
         $product1 = Product::factory()->create();
 
         $char = Characteristic::factory()->create(['category_id' => $product1->category_id]);
-        DB::table('characteristic_product')->insert(['product_id' => $product1->id, 'characteristic_id' => $char->id, 'value' => 'unique' ]);
+        DB::table('characteristic_product')->insert(['product_id' => $product1->id, 'characteristic_id' => $char->id, 'value' => 'unique']);
 
         $this->assertDatabaseCount('characteristic_product', 1);
 
@@ -119,16 +109,9 @@ class CharacteristicTest extends TestCase
         $this->withoutExceptionHandling();
         $this->expectExceptionMessage('UNIQUE');
 
-        $category = Category::factory()->create();
+        $product = Product::factory()->create();
 
-        DB::table('characteristics')->insert([
-            'name' => 'test',
-            'category_id' => $category->id
-        ] );
-
-        DB::table('characteristics')->insert([
-            'name' => 'test',
-            'category_id' => $category->id
-        ] );
+        $this->createCharacteristicsForProduct($product);
+        $this->createCharacteristicsForProduct($product);
     }
 }
