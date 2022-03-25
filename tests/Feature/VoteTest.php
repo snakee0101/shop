ede9cea648a7101408c705367a6520d503f58432
Danil <snakee0101@gmail.com>
2022-03-25 20:19:51 +0200

    Feature\VoteTest refactoring

--- tests/Feature/VoteTest.php
+++ tests/Feature/VoteTest.php
@@ -2,44 +2,71 @@
 
 namespace Tests\Feature;
 
+use App\Models\Question;
 use App\Models\Review;
-use App\Models\Vote;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class VoteTest extends TestCase
 {
-    public function test_review_could_be_votes()
+    private $review, $question, $review_vote_data, $question_vote_data;
+
+    protected function setUp(): void
     {
-        $review = Review::factory()->create();
+        parent::setUp();
 
-        $this->actingAs( $review->author );
-        $this->post( route('vote.store'), [
-            'object_id' => $review->id,
-            'object_type' => $review::class,
-            'value' => -1
-        ]);
+        $this->review = Review::factory()->create();
+        $this->question = Question::factory()->create();
 
-        $this->assertDatabaseHas('votes', [
-            'object_id' => $review->id,
-            'object_type' => $review::class,
+        $this->review_vote_data = [
+            'object_id' => $this->review->id,
+            'object_type' => Review::class,
             'value' => -1
-        ]);
+        ];
+
+        $this->question_vote_data = [
+            'object_id' => $this->question->id,
+            'object_type' => Question::class,
+            'value' => -1
+        ];
     }
 
-    public function test_when_object_is_voted_statistics_is_returned()
+    public function test_review_could_be_votes()
     {
-        $review = Review::factory()->create();
+        $this->actingAs( $this->review->author );
 
-        $this->actingAs( $review->author );
-        $this->post( route('vote.store'), [
-            'object_id' => $review->id,
-            'object_type' => $review::class,
-            'value' => -1
-        ])->assertJson([
-            'for_count' => 0,
-            'against_count' => 1
-        ]);
+        $this->post( route('vote.store'), $this->review_vote_data);
+        $this->assertDatabaseHas('votes', $this->review_vote_data);
+    }
+
+    public function test_when_review_is_voted_statistics_is_returned()
+    {
+        $this->actingAs( $this->review->author );
+
+        $this->post( route('vote.store'), $this->review_vote_data)
+             ->assertJson([
+                'for_count' => 0,
+                'against_count' => 1
+             ]);
+    }
+
+    public function test_question_could_be_votes()
+    {
+        $this->actingAs( $this->question->author );
+
+        $this->post( route('vote.store'), $this->question_vote_data);
+        $this->assertDatabaseHas('votes', $this->question_vote_data);
+    }
+
+    public function test_when_question_is_voted_statistics_is_returned()
+    {
+        $this->actingAs( $this->question->author );
+
+        $this->post( route('vote.store'), $this->question_vote_data)
+            ->assertJson([
+                'for_count' => 0,
+                'against_count' => 1
+            ]);
     }
 }
