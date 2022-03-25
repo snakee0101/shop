75c0ee91c7dbf3c397f156aad8ac6d8a298e8f7a
Danil <snakee0101@gmail.com>
2022-03-25 14:16:53 +0200

    Unit/ReplyTest - removed temporary variables

--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -13,7 +13,7 @@ class ReplyTest extends TestCase
     public function test_review_has_many_replies()
     {
         $review = Review::factory()->create();
-        $replies = Reply::factory()->withObject($review)->count(2)->create();
+        Reply::factory()->withObject($review)->count(2)->create();
 
         $this->assertInstanceOf(Reply::class, $review->fresh()->replies[0]);
         $this->assertCount(2, $review->fresh()->replies);
@@ -22,7 +22,7 @@ class ReplyTest extends TestCase
     }public function test_question_has_many_replies()
     {
         $question = Question::factory()->create();
-        $replies = Reply::factory()->withObject($question)->count(2)->create();
+        Reply::factory()->withObject($question)->count(2)->create();
 
         $this->assertInstanceOf(Reply::class, $question->fresh()->replies[0]);
         $this->assertCount(2, $question->fresh()->replies);
@@ -30,16 +30,16 @@ class ReplyTest extends TestCase
 
     public function test_reply_has_an_author()
     {
-        $review = Review::factory()->create();
-        $reply = Reply::factory()->withObject($review)->create();
+        $reply = Reply::factory()->withObject( Review::factory()->create() )
+                                 ->create();
 
         $this->assertInstanceOf(User::class, $reply->author);
     }
 
     public function test_replied_object_is_available()
     {
-        $review = Review::factory()->create();
-        $reply = Reply::factory()->withObject($review)->create();
+        $reply = Reply::factory()->withObject( Review::factory()->create() )
+                                 ->create();
 
         $this->assertInstanceOf(Review::class, $reply->object);
     }
