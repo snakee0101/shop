96291e425b9e7b8f63a37f31a0f43f4f21e8ef2a
Danil <snakee0101@gmail.com>
2022-03-28 22:33:38 +0300

    Review relations are eager-loaded

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -12,7 +12,7 @@ class Review extends Model
 
     protected $guarded = [];
     protected $appends = ['is_voted', 'vote', 'vote_statistics'];
-    protected $with = ['votes'];
+    protected $with = ['votes', 'photos', 'videos', 'replies', 'author'];
 
     public function author()
     {
