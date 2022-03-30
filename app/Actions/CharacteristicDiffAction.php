b3e93116176bc9f02acda35e320119d584e45751
Danil <snakee0101@gmail.com>
2022-03-30 23:27:19 +0300

    Characteristics diff - optimized uniqueness filtering

--- app/Actions/CharacteristicDiffAction.php
+++ app/Actions/CharacteristicDiffAction.php
@@ -26,10 +26,9 @@ class CharacteristicDiffAction
         //[ char_id => [char_value_1, char_value_2, ...], ... ]   - extracted only characteristic value
 
         $unique_chars = $grouped_char_values->reject(function ($char_values) {
-            $number_of_characteristics_in_row = $char_values->count();
-            $number_of_duplicates_in_row = $char_values->duplicates()->count() + 1;
+            $value_of_some_char = $char_values[0];
 
-            return $number_of_characteristics_in_row === $number_of_duplicates_in_row;  //TODO: return $char_values->unique()->isEmpty()  //if unique is empty - then row consists of duplicates
+            return $char_values->every( fn($value) => $value_of_some_char === $value );  //if every item equals to first item - then all items are duplicates and row must be removed
         });  //removed characteristics that have the same value across all products (for example, this will be removed 1 => [5, 5, 5], but this will not - 2 => [4, 4, 3] )
 
         $unique_characteristics_model_ids = $unique_chars->keys(); //extracted characteristics model ids
