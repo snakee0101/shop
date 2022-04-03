e5a191b248dbfb651fbe103e8eeeaed2d55f2dfd
Danil <snakee0101@gmail.com>
2022-04-03 21:27:50 +0300

    WishlistComponent.vue refactoring

--- resources/js/components/WishlistComponent.vue
+++ resources/js/components/WishlistComponent.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="card mb-5">
-        <div class="modal fade" :id="'move_wishlist_modal_' + wishlist_object.id" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" :id="'move_wishlist_modal_' + wishlist_object.id" tabindex="-1" role="dialog"
+             aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Move selected products to another wishlist</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close" :ref="'close_' + wishlist_object.id">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"
+                                :ref="'close_' + wishlist_object.id">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
                         <div class="form-group">
                             <label for="wishlist_name">Name of new wishlist</label><br>
-                            <select class="form-control form-select" id="wishlist_name" v-model="wishlist_id_to_move_to">
+                            <select class="form-control form-select" id="wishlist_name"
+                                    v-model="wishlist_id_to_move_to">
                                 <option v-for="w in other_wishlists" :value="w.id">{{ w.name }}</option>
                             </select>
                         </div>
@@ -26,7 +29,8 @@
         </div>
 
 
-        <div class="card-header d-flex align-content-center" :class="wishlist.is_active ? 'bg-dark text-white font-weight-bold' : ''">
+        <div class="card-header d-flex align-content-center"
+             :class="wishlist.is_active ? 'bg-dark text-white font-weight-bold' : ''">
             <p class="d-inline-block m-0 pt-2 mr-auto" v-if="is_renaming">
                 <input type="text" v-model="new_name">
                 <button class="btn btn-success btn-sm" @click="confirm_rename()">Ok</button>
@@ -44,14 +48,18 @@
         <div class="card-body p-0">
             <div class="d-flex align-content-center mb-3 p-4">
                 <div class="d-inline-block flex-grow-1" v-if="user_object.id">
-                    <button class="btn btn-success" @click="selectAll()" v-text="all_selected ? 'Deselect All' : 'Select All'"></button>
-                    <button class="btn btn-warning" data-toggle="modal" :data-target="'#move_wishlist_modal_' + wishlist_object.id">Move</button>
+                    <button class="btn btn-success" @click="selectAll()"
+                            v-text="all_selected ? 'Deselect All' : 'Select All'"></button>
+                    <button class="btn btn-warning" data-toggle="modal"
+                            :data-target="'#move_wishlist_modal_' + wishlist_object.id">Move
+                    </button>
                     <button class="btn btn-danger" @click="remove_from_wishlist()">Delete</button>
                     <button class="btn btn-info" @click="copyURL()">Copy URL</button>
                 </div>
                 <div>
                     <div class="btn-group">
-                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false">
                             Sort by
                         </button>
                         <div class="dropdown-menu">
@@ -96,123 +104,105 @@ export default {
         window.events.$on('toggled_product_wishlist_selection', this.save_selection);
     },
     mounted() {
-        this.other_wishlists = this.wishlists.filter( wishlist => wishlist.id !== this.wishlist_object.id );
+        this.other_wishlists = this.wishlists.filter(wishlist => wishlist.id !== this.wishlist_object.id);
     },
     data() {
         return {
-          'wishlist_object' : this.wishlist,
-          'user_object' : this.user,
-          'selected_product_ids' : [],
-          'all_selected' : false,
-          'is_renaming' : false,
-          'new_name' : this.wishlist.name,
-          'other_wishlists' : {},
-          'wishlist_id_to_move_to' : 0
+            'wishlist_object': this.wishlist,
+            'user_object': this.user,
+            'selected_product_ids': [],
+            'all_selected': false,
+            'is_renaming': false,
+            'new_name': this.wishlist.name,
+            'other_wishlists': {},
+            'wishlist_id_to_move_to': 0
         };
     },
     computed: {
         total() {
-            if(this.selected_product_ids.length === 0)
+            if (this.selected_product_ids.length === 0)
                 return this.wishlist_object.products.map(product => product.PriceWithDiscount)
-                                                         .reduce( (prev_price, current_price) => prev_price + current_price, 0);
+                    .reduce((prev_price, current_price) => prev_price + current_price, 0);
             else
-                return this.wishlist_object.products.filter( product => this.selected_product_ids.indexOf( product.id ) !== -1 )
-                                                         .map(product => product.PriceWithDiscount)
-                                                         .reduce( (prev_price, current_price) => prev_price + current_price, 0);
+                return this.wishlist_object.products.filter(product => this.selected_product_ids.indexOf(product.id) !== -1)
+                    .map(product => product.PriceWithDiscount)
+                    .reduce((prev_price, current_price) => prev_price + current_price, 0);
         }
     },
     methods: {
-       add_all_to_cart()
-       {
+        add_all_to_cart() {
             window.events.$emit('add_all_to_cart', this.wishlist_object.id);
-       },
-       move_to_wishlist()
-       {
-           if(this.wishlist_id_to_move_to === 0) {
-               alert('No wishlist is selected');
-           } else {
-               if(this.selected_product_ids.length === 0) {
-                   alert('No products are selected');
-               } else {
-                   this.selected_product_ids.map(this.move_to_wishlist_callback);
-                   location.reload();
-               }
-           }
-       },
-       move_to_wishlist_callback(product_id)
-       {
-           axios.post(`wishlist/${this.wishlist_object.id}/${product_id}/move`, {
-               'move_to' : this.wishlist_id_to_move_to
-           });
-       },
-       remove_from_wishlist()
-       {
-           this.selected_product_ids.forEach(this.remove_from_wishlist_callback);
-           this.selected_product_ids.splice(0, this.selected_product_ids.length);
-       },
-       remove_from_wishlist_callback(product_id)
-       {
-           axios.post(`/wishlist/${this.wishlist_object.id}/${product_id}`);
-           this.wishlist_object.products = this.wishlist_object.products.filter( product => product.id !== product_id );
-       },
-       save_selection(wishlist_id, product_id, selected)
-       {
-           if(wishlist_id == this.wishlist_object.id) {
-                if(selected)
-                    this.selected_product_ids.push(product_id);
-                else
-                    this.selected_product_ids.splice(this.selected_product_ids.indexOf(product_id), 1);
-           }
-       },
-       selectAll()
-       {
-           this.all_selected = !this.all_selected;
+        },
+        move_to_wishlist() {
+            if (this.wishlist_id_to_move_to === 0)
+                return alert('No wishlist is selected');
 
-           if(this.all_selected)
-               this.selected_product_ids = this.wishlist_object.products.map( product => product.id );
-            else
-               this.selected_product_ids = [];
+            if (this.selected_product_ids.length === 0)
+                return alert('No products are selected');
 
-           window.events.$emit('toggle_select_all_products_in_a_wishlist', this.wishlist_object.id, this.selected_product_ids);
-       },
-       setDefault()
-       {
-           axios.get(`/wishlist/${this.wishlist_object.id}/set_default`)
-                .then( () => location.reload() );
-       },
-       start_rename()
-       {
-           this.new_name = this.wishlist_object.name;
-           this.is_renaming = true;
-       },
-       confirm_rename()
-       {
-           if(this.new_name === '') {
-               alert('new wishlist name is required');
-           } else {
-               axios.put(`/wishlist/${this.wishlist_object.id}`, {
-                   'name' : this.new_name
-               }).then(this.rename_wishlist)
-                 .catch(
-                   error => alert('wishlist with this name is already exists')
-               );
-           }
-       },
-       rename_wishlist()
-       {
-           this.wishlist_object.name = this.new_name;
-           this.is_renaming = false;
-       },
-       delete_wishlist()
-       {
-           axios.delete(`/wishlist/${this.wishlist_object.id}`)
+            this.selected_product_ids.map(this.move_to_wishlist_callback);
+            location.reload();
+        },
+        move_to_wishlist_callback(product_id) {
+            axios.post(`wishlist/${this.wishlist_object.id}/${product_id}/move`, {
+                'move_to': this.wishlist_id_to_move_to
+            });
+        },
+        remove_from_wishlist() {
+            this.selected_product_ids.forEach(this.remove_from_wishlist_callback);
+            this.selected_product_ids.splice(0, this.selected_product_ids.length);
+        },
+        remove_from_wishlist_callback(product_id) {
+            axios.post(`/wishlist/${this.wishlist_object.id}/${product_id}`);
+            this.wishlist_object.products = this.wishlist_object.products.filter(product => product.id !== product_id);
+        },
+        save_selection(wishlist_id, product_id, selected) {
+            if (wishlist_id != this.wishlist_object.id)
+                return;
+
+            if (selected)
+                return this.selected_product_ids.push(product_id);
+
+            this.selected_product_ids.splice(this.selected_product_ids.indexOf(product_id), 1);
+        },
+        selectAll() {
+            this.all_selected = !this.all_selected;
+
+            this.selected_product_ids = this.all_selected ? this.wishlist_object.products.map(product => product.id) : [];
+
+            window.events.$emit('toggle_select_all_products_in_a_wishlist', this.wishlist_object.id, this.selected_product_ids);
+        },
+        setDefault() {
+            axios.get(`/wishlist/${this.wishlist_object.id}/set_default`)
                 .then(() => location.reload());
-       },
-       copyURL()
-       {
-           navigator.clipboard.writeText( this.url )
-                              .then( () => alert('wishlist URL was successfully copied') );
-       }
+        },
+        start_rename() {
+            this.new_name = this.wishlist_object.name;
+            this.is_renaming = true;
+        },
+        confirm_rename() {
+            if (this.new_name === '')
+                return alert('new wishlist name is required');
+
+            axios.put(`/wishlist/${this.wishlist_object.id}`, {
+                'name': this.new_name
+            }).then(this.rename_wishlist)
+                .catch(
+                    error => alert('wishlist with this name is already exists')
+                );
+        },
+        rename_wishlist() {
+            this.wishlist_object.name = this.new_name;
+            this.is_renaming = false;
+        },
+        delete_wishlist() {
+            axios.delete(`/wishlist/${this.wishlist_object.id}`)
+                .then(() => location.reload());
+        },
+        copyURL() {
+            navigator.clipboard.writeText(this.url)
+                .then(() => alert('wishlist URL was successfully copied'));
+        }
     }
 }
 </script>
