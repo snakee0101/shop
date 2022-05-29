941f207b86b80f95bb5add36f048c76edc8e4feb
Danil <snakee0101@gmail.com>
2022-05-29 21:39:28 +0300

    news subscription - if email exists - exception is raised

--- tests/Feature/NewsSubscriptionTest.php
+++ tests/Feature/NewsSubscriptionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\NewsSubscriber;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,5 +17,16 @@ class NewsSubscriptionTest extends TestCase
         $this->assertDatabaseHas('news_subscribers', [
             'email' => 'new@email.com'
         ]);
+    }
+
+    public function test_if_subscriber_email_is_exists_exception_is_raised()
+    {
+        $this->withoutExceptionHandling()
+             ->expectExceptionMessage('UNIQUE');
+
+        NewsSubscriber::factory()->create(['email' => 'new@email.com']);
+
+        $this->post( route('news.subscribe', 'new@email.com') )
+            ->assertStatus(200);
     }
 }
