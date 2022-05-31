f42a9345f36e99fe56b3f0697c4b9bedaa2635ee
Danil <snakee0101@gmail.com>
2022-05-31 21:46:47 +0300

    created contact form message model and migration

---
+++ database/migrations/2022_05_31_183920_create_contact_form_message_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactFormMessageTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('contact_form_messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('subject');
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('contact_form_messages');
+    }
+}
