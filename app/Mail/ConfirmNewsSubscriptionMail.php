223d74adc864e66c98f0a0fe1cc641e9b74983d5
Danil <snakee0101@gmail.com>
2022-06-02 14:39:38 +0300

    mailables are queueable

--- app/Mail/ConfirmNewsSubscriptionMail.php
+++ app/Mail/ConfirmNewsSubscriptionMail.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\URL;
 
-class ConfirmNewsSubscriptionMail extends Mailable
+class ConfirmNewsSubscriptionMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
--- app/Mail/ContactFormMessageRepliedMail.php
+++ app/Mail/ContactFormMessageRepliedMail.php
@@ -4,10 +4,11 @@ namespace App\Mail;
 
 use App\Models\ContactFormMessage;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class ContactFormMessageRepliedMail extends Mailable
+class ContactFormMessageRepliedMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
