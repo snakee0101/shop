f8adce77b05d6cedd3dee7ae3c5484c2b8101072
Danil <snakee0101@gmail.com>
2022-06-08 18:01:55 +0300

    created News->category relation

--- app/Models/News.php
+++ app/Models/News.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(NewsCategory::class, 'news_category_id');
+    }
 }
--- tests/Feature/NewsTest.php
+++ tests/Feature/NewsTest.php
@@ -3,14 +3,17 @@
 namespace Tests\Feature;
 
 use App\Models\News;
+use App\Models\NewsCategory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class NewsTest extends TestCase
 {
-    public function test_example()
+    public function test_news_has_a_category()
     {
+        $news = News::factory()->create();
 
+        $this->assertInstanceOf(NewsCategory::class, $news->category);
     }
 }
