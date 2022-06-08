fb1a961997ae9e19faae5b89e0d0a0529fec1668
Danil <snakee0101@gmail.com>
2022-06-08 15:17:51 +0300

    filled news categories migration with data

---
+++ database/migrations/2022_06_08_121444_create_news_categories_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsCategoriesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('news_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('parent_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('news_categories');
+    }
+}
