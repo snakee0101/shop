68ab3ce275275584cf424d4190339a78a3f89eed
Danil <snakee0101@gmail.com>
2022-06-11 11:08:28 +0300

    popular tags are successfully displayed on the news page

--- resources/views/news-article.blade.php
+++ resources/views/news-article.blade.php
@@ -208,7 +208,9 @@
                         <div class="widget-tags widget"><h4 class="widget__title">Popular Tags</h4>
                             <div class="tags tags--lg">
                                 <div class="tags__list">
-                                    <a href="#">Promotion</a>
+                                    @foreach($popular_tags as $popular_tag)
+                                        <a href="{{ route('news.index', ['tag' => $popular_tag->id])  }}">{{ $popular_tag->name }}</a>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
