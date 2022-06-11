a50aa7ad63d6efc7cca5b74803aa25032ad5a742
Danil <snakee0101@gmail.com>
2022-06-11 15:06:55 +0300

    extracted news card partial

--- resources/views/news-index.blade.php
+++ resources/views/news-index.blade.php
@@ -32,36 +32,7 @@
                         <div class="posts-view__list posts-list posts-list--layout--list">
                             <div class="posts-list__body">
                                 @foreach($news as $news_article)
-                                    <div class="posts-list__item">
-                                        <div class="post-card post-card--layout--list post-card--size--nl">
-                                            <div class="post-card__image" style="width: 250px">
-                                                <a href="{{ route('news.show', $news_article) }}">
-                                                    <img src="{{ $news_article->main_image_url }}" alt="">
-                                                </a>
-                                            </div>
-                                            <div class="post-card__info">
-                                                <div class="post-card__category">
-                                                    <a href="{{ route('news.index', ['category' => $news_article->category->id])  }}">
-                                                        {{ $news_article->category->name }}
-                                                    </a>
-                                                </div>
-                                                <div class="post-card__date" style="color: #f00">{{ $news_article->created_at->format('M d, Y') }}</div>
-
-                                                <div class="post-card__name w-100">
-                                                    <a href="{{ route('news.show', $news_article) }}">
-                                                        {{ $news_article->caption }}
-                                                    </a>
-                                                </div>
-
-                                                <div class="post-card__content">
-                                                    {!! $news_article->excerpt() !!}
-                                                </div>
-                                                <div class="post-card__read-more">
-                                                    <a href="{{ route('news.show', $news_article) }}" class="btn btn-secondary btn-sm">Read More</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    @include('partials.news.card', ['news_article' => $news_article])
                                 @endforeach
                             </div>
                         </div>
