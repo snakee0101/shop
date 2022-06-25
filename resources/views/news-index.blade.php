a2b5d72d35241b8a02158e89ab5540d0a9e41f31
Danil <snakee0101@gmail.com>
2022-06-25 23:19:35 +0300

    news search query is saved to the session

--- resources/views/news-index.blade.php
+++ resources/views/news-index.blade.php
@@ -2,15 +2,17 @@
 
 @section('body')
     <div class="page-header">
-        @if(session()->has('news_search_tag_id') || session()->has('news_search_category_id') || session()->has('news_search_query'))
-            <div class="page-header__container container">
+        <div class="page-header__container container">
+            @if(session()->has('news_search_tag_id') || session()->has('news_search_category_id') || session()->has('news_search_query'))
                 <p style="font-size: 1.3em" class="d-inline">Filters:</p>
 
                 @if( session()->has('news_search_category_id') )
                     <span class="badge bg-warning p-2 m-2" style="font-size: 1em">
                         <a href="{{ route('news.search.clear', 'news_search_category_id') }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000" class="bi bi-x-lg" viewBox="0 0 16 16">
-                              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000"
+                                 class="bi bi-x-lg" viewBox="0 0 16 16">
+                              <path
+                                  d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                             </svg>
                         </a>
                         <b>Category</b>: {{ \App\Models\NewsCategory::find( session('news_search_category_id'))->name }}
@@ -20,8 +22,10 @@
                 @if( session()->has('news_search_tag_id') )
                     <span class="badge bg-warning p-2 m-2" style="font-size: 1em">
                         <a href="{{ route('news.search.clear', 'news_search_tag_id') }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000" class="bi bi-x-lg" viewBox="0 0 16 16">
-                              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000"
+                                 class="bi bi-x-lg" viewBox="0 0 16 16">
+                              <path
+                                  d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                             </svg>
                         </a>
                         <b>Tag</b>: {{ \App\Models\Tag::find( session('news_search_tag_id'))->name }}
@@ -31,15 +35,17 @@
                 @if( session()->has('news_search_query') )
                     <span class="badge bg-warning p-2 m-2" style="font-size: 1em">
                         <a href="{{ route('news.search.clear', 'news_search_query') }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000" class="bi bi-x-lg" viewBox="0 0 16 16">
-                              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000"
+                                 class="bi bi-x-lg" viewBox="0 0 16 16">
+                              <path
+                                  d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                             </svg>
                         </a>
                         <b>Search query</b>: {{ session('news_search_query') }}
                     </span>
                 @endif
-            </div>
-        @endif
+            @endif
+        </div>
     </div>
 
     <div class="container">
@@ -64,7 +70,8 @@
                 <div class="block block-sidebar block-sidebar--position--end">
                     <div class="block-sidebar__item">
                         <div class="widget-search">
-                            <form class="widget-search__body"><input class="widget-search__input"
+                            <form class="widget-search__body" action="{{ route('news.index') }}">
+                                <input class="widget-search__input" name="search"
                                                                      placeholder="Blog search..." type="text"
                                                                      autocomplete="off" spellcheck="false">
                                 <button class="widget-search__button" type="submit">
