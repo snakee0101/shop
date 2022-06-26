ced09ef831a241a838867bba8e885717172a4faf
Danil <snakee0101@gmail.com>
2022-06-26 17:58:50 +0300

    changed form type for news.create form

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -10,7 +10,7 @@
             </div>
             <!-- /.card-header -->
             <!-- form start -->
-            <form action="{{ route('news.store') }}" method="post">
+            <form action="{{ route('news.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 <div class="card-body">
                     <div class="form-group">
